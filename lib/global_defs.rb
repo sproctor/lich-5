289c21394f692a8c787e7646b166e7998ea665ea
Ryan P. McKinnon <15917743+mrhoribu@users.noreply.github.com>
2022-05-29 12:21:07 -0400

    Update global_defs.rb

--- lib/global_defs.rb
+++ lib/global_defs.rb
@@ -2164,7 +2164,7 @@ def do_client(client_string)
       update_parameter = $1.dup
       require 'lib/update.rb'
       Lich::Util::Update.request("#{update_parameter}")
-    elsif cmd =~ /^(?:lich5-update|l5u)/i
+    elsif cmd =~ /^(?:lich5-update|l5u)$/i
       require 'lib/update.rb'
       Lich::Util::Update.request("--help")
     elsif cmd =~ /^help$/i
